f6d27b87b27db30d03b78e151ea4a921c78ec84d
Wendel Duarte <wendel_duarte@outlook.com>
2020-08-22 17:03:25 -0300

    First commit

---
+++ src/test/java/com/wendel/osworks/OsworksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wendel.osworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsworksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

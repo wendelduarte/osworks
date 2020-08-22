fb8efd68650d94640f0bbcffb6af57b876621ef2
Wendel Duarte <wendel_duarte@outlook.com>
2020-08-22 17:32:29 -0300

    Add repository

--- src/main/java/com/wendel/osworks/api/controller/ClienteController.java
+++ src/main/java/com/wendel/osworks/api/controller/ClienteController.java
@@ -2,22 +2,21 @@ package com.wendel.osworks.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wendel.osworks.domain.model.Cliente;
+import com.wendel.osworks.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
-
-	@PersistenceContext
-	private EntityManager manager;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		return manager.createQuery("from Cliente", Cliente.class).getResultList();
+		return clienteRepository.findAll();
 	}
 }

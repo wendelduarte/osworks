e3b465bc917c4cbad3ccbcdc65f349ff6767786e
Wendel Duarte <wendel_duarte@outlook.com>
2020-08-25 00:28:16 -0300

    Adicionar busca por id na ordem de servico

--- src/main/java/com/wendel/osworks/api/controller/OrdemServicoController.java
+++ src/main/java/com/wendel/osworks/api/controller/OrdemServicoController.java
@@ -1,11 +1,15 @@
 package com.wendel.osworks.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +36,19 @@ public class OrdemServicoController {
 		return gestaoOrdemServico.criar(ordemServico);
 	}
 	
+	@GetMapping
 	public List<OrdemServico> listar(){
 		return ordemServicoRepository.findAll();
+	}
+	
+	@GetMapping("/{ordemServicoId}")
+	public ResponseEntity<OrdemServico> buscar(@PathVariable Long ordemServicoId){
+		Optional<OrdemServico> ordemServico = ordemServicoRepository.findById(ordemServicoId);
+		
+		if(ordemServico.isPresent()) {
+			return ResponseEntity.ok(ordemServico.get());
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 }
